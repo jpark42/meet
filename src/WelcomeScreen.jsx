ab2b02bbc26147358be9a8d4bafaec10bf58f139
Johnny Park <110794898+jpark42@users.noreply.github.com>
2023-04-04 19:39:55 -0400

    WelcomeScreen styling

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -4,15 +4,15 @@ import "./WelcomeScreen.css";
 function WelcomeScreen(props) {
   return props.showWelcomeScreen ? (
     <div className="WelcomeScreen">
-      <h1>Welcome to the You Herd app</h1>
+      <h1>Welcome to the Meet app</h1>
       <h4>
         Log in to see upcoming events around the world for full-stack developers
       </h4>
       <div className="button_cont" align="center">
-        <div class="google-btn">
-          <div class="google-icon-wrapper">
+        <div className="google-btn">
+          <div className="google-icon-wrapper">
             <img
-              class="google-icon"
+              className="google-icon"
               src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
               alt="Google sign-in"
             />
@@ -22,7 +22,7 @@ function WelcomeScreen(props) {
               props.getAccessToken();
             }}
             rel="nofollow noopener"
-            class="btn-text"
+            className="btn-text"
           >
             <b>Sign in with google</b>
           </button>
