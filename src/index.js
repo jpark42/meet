01dc44ae0ca43c0396c4724eee3da84887dbb59d
Johnny Park <110794898+jpark42@users.noreply.github.com>
2023-04-11 09:14:41 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-/*
+/* Free trial for atatus now expired
 import * as atatus from 'atatus-spa';
 
 atatus.config('758c0801e5154c1cadb51aa65394196f').install();
